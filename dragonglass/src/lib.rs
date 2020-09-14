1ee2e8197004741598e17cf4fba4048080b129c5
Matthew J. Berger <matthewberger@nevada.unr.edu>
2020-09-14 16:49:39 -0600

    Add clippy lints

--- dragonglass/src/lib.rs
+++ dragonglass/src/lib.rs
@@ -1,3 +1,23 @@
+#![warn(
+    clippy::all,
+    clippy::as_conversions,
+    clippy::cognitive_complexity,
+    clippy::dbg_macro,
+    clippy::default_trait_access,
+    clippy::expect_used,
+    clippy::if_not_else,
+    clippy::indexing_slicing,
+    clippy::inefficient_to_string,
+    clippy::needless_borrow,
+    clippy::todo,
+    clippy::too_many_lines,
+    clippy::unreachable,
+    clippy::unused_self,
+    clippy::use_self,
+    clippy::wildcard_dependencies,
+    clippy::wildcard_imports
+)]
+
 pub use self::device::RenderingDevice;
 
 mod core;
