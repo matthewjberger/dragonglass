3c810fe5690bd4505a55730b5dadb64b3d57aedd
Matthew J. Berger <matthewberger@nevada.unr.edu>
2020-09-18 22:43:41 -0600

    Add postprocessing shaders

--- dragonglass/src/forward.rs
+++ dragonglass/src/forward.rs
@@ -172,8 +172,6 @@ impl ColorRenderTarget {
 }
 
 pub struct ForwardSwapchain {
-    pub color_target: ColorRenderTarget,
-    pub depth_target: DepthRenderTarget,
     pub render_pass: Arc<RenderPass>,
     pub framebuffers: Vec<Framebuffer>,
     pub swapchain: Swapchain,
@@ -185,33 +183,12 @@ impl ForwardSwapchain {
         let (swapchain, swapchain_properties) = context.create_swapchain(dimensions)?;
         let surface_format = swapchain_properties.surface_format.format;
 
-        let extent = swapchain_properties.extent;
-        let extent = vk::Extent3D::builder()
-            .width(extent.width)
-            .height(extent.height)
-            .depth(1)
-            .build();
+        let render_pass = Self::render_pass(context.logical_device.clone(), surface_format)?;
 
-        let allocator = context.allocator.clone();
-        let device = context.logical_device.clone();
-
-        let depth_format = context.determine_depth_format(
-            vk::ImageTiling::OPTIMAL,
-            vk::FormatFeatureFlags::DEPTH_STENCIL_ATTACHMENT,
-        )?;
-
-        let depth_target =
-            DepthRenderTarget::new(device.clone(), allocator.clone(), extent, depth_format)?;
-        let color_target = ColorRenderTarget::new(device, allocator, extent, surface_format)?;
-        let render_pass = Self::render_pass(
-            context.logical_device.clone(),
-            surface_format,
-            depth_target.format,
-        )?;
         let framebuffers = swapchain
             .images
             .iter()
-            .map(|image| [image.view.handle, depth_target.view.handle])
+            .map(|image| [image.view.handle])
             .map(|attachments| {
                 let create_info = vk::FramebufferCreateInfo::builder()
                     .render_pass(render_pass.handle)
@@ -222,9 +199,8 @@ impl ForwardSwapchain {
                 Framebuffer::new(context.logical_device.clone(), create_info)
             })
             .collect::<Result<Vec<_>, _>>()?;
+
         let forward_swapchain = Self {
-            depth_target,
-            color_target,
             render_pass: Arc::new(render_pass),
             framebuffers,
             swapchain,
@@ -233,31 +209,18 @@ impl ForwardSwapchain {
         Ok(forward_swapchain)
     }
 
-    fn render_pass(
-        device: Arc<LogicalDevice>,
-        color_format: vk::Format,
-        depth_format: vk::Format,
-    ) -> Result<RenderPass> {
+    fn render_pass(device: Arc<LogicalDevice>, color_format: vk::Format) -> Result<RenderPass> {
         let color_attachment_description = Self::color_attachment_description(color_format);
-        let depth_attachment_description = Self::depth_attachment_description(depth_format);
-        let attachment_descriptions = [
-            color_attachment_description.build(),
-            depth_attachment_description.build(),
-        ];
+        let attachment_descriptions = [color_attachment_description.build()];
 
         let color_attachment_reference = vk::AttachmentReference::builder()
             .attachment(0)
             .layout(vk::ImageLayout::COLOR_ATTACHMENT_OPTIMAL);
         let color_attachment_references = [color_attachment_reference.build()];
 
-        let depth_attachment_reference = vk::AttachmentReference::builder()
-            .attachment(1)
-            .layout(vk::ImageLayout::DEPTH_STENCIL_ATTACHMENT_OPTIMAL);
-
         let subpass_description = vk::SubpassDescription::builder()
             .pipeline_bind_point(vk::PipelineBindPoint::GRAPHICS)
-            .color_attachments(&color_attachment_references)
-            .depth_stencil_attachment(&depth_attachment_reference);
+            .color_attachments(&color_attachment_references);
         let subpass_descriptions = [subpass_description.build()];
 
         let subpass_dependencies = Self::subpass_dependencies();
@@ -282,31 +245,31 @@ impl ForwardSwapchain {
             .final_layout(vk::ImageLayout::PRESENT_SRC_KHR)
     }
 
-    fn depth_attachment_description<'a>(
-        format: vk::Format,
-    ) -> vk::AttachmentDescriptionBuilder<'a> {
-        vk::AttachmentDescription::builder()
-            .format(format)
-            .samples(vk::SampleCountFlags::TYPE_1)
-            .load_op(vk::AttachmentLoadOp::CLEAR)
-            .store_op(vk::AttachmentStoreOp::DONT_CARE)
-            .stencil_load_op(vk::AttachmentLoadOp::DONT_CARE)
-            .stencil_store_op(vk::AttachmentStoreOp::DONT_CARE)
-            .initial_layout(vk::ImageLayout::UNDEFINED)
-            .final_layout(vk::ImageLayout::DEPTH_STENCIL_ATTACHMENT_OPTIMAL)
-    }
-
-    fn subpass_dependencies() -> [vk::SubpassDependency; 1] {
-        let subpass_dependency = vk::SubpassDependency::builder()
-            .src_subpass(vk::SUBPASS_EXTERNAL)
-            .dst_subpass(0)
-            .src_stage_mask(vk::PipelineStageFlags::COLOR_ATTACHMENT_OUTPUT)
-            .src_access_mask(vk::AccessFlags::empty())
-            .dst_stage_mask(vk::PipelineStageFlags::COLOR_ATTACHMENT_OUTPUT)
-            .dst_access_mask(
-                vk::AccessFlags::COLOR_ATTACHMENT_READ | vk::AccessFlags::COLOR_ATTACHMENT_WRITE,
-            );
-        [subpass_dependency.build()]
+    fn subpass_dependencies() -> [vk::SubpassDependency; 2] {
+        [
+            vk::SubpassDependency::builder()
+                .src_subpass(vk::SUBPASS_EXTERNAL)
+                .dst_subpass(0)
+                .src_stage_mask(vk::PipelineStageFlags::BOTTOM_OF_PIPE)
+                .dst_stage_mask(vk::PipelineStageFlags::COLOR_ATTACHMENT_OUTPUT)
+                .src_access_mask(vk::AccessFlags::MEMORY_READ)
+                .dst_access_mask(
+                    vk::AccessFlags::COLOR_ATTACHMENT_READ
+                        | vk::AccessFlags::COLOR_ATTACHMENT_WRITE,
+                )
+                .build(),
+            vk::SubpassDependency::builder()
+                .src_subpass(0)
+                .dst_subpass(vk::SUBPASS_EXTERNAL)
+                .src_stage_mask(vk::PipelineStageFlags::COLOR_ATTACHMENT_OUTPUT)
+                .dst_stage_mask(vk::PipelineStageFlags::BOTTOM_OF_PIPE)
+                .src_access_mask(
+                    vk::AccessFlags::COLOR_ATTACHMENT_READ
+                        | vk::AccessFlags::COLOR_ATTACHMENT_WRITE,
+                )
+                .dst_access_mask(vk::AccessFlags::MEMORY_READ)
+                .build(),
+        ]
     }
 }
 
@@ -485,8 +448,8 @@ impl PostProcessingPipeline {
 
     fn shader_paths() -> Result<ShaderPathSet> {
         let shader_path_set = ShaderPathSetBuilder::default()
-            .vertex("dragonglass/shaders/triangle/fullscreen_triangle.vert.spv")
-            .fragment("dragonglass/shaders/triangle/post_process.frag.spv")
+            .vertex("dragonglass/shaders/postprocess/fullscreen_triangle.vert.spv")
+            .fragment("dragonglass/shaders/postprocess/postprocess.frag.spv")
             .build()
             .map_err(|error| anyhow!("{}", error))?;
         Ok(shader_path_set)
