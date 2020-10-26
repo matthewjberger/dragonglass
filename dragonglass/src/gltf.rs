b93dcb1e9459a5f63da1e496d62f7ab09a0f11b6
matthewjberger <matthewjberger@nevada.unr.edu>
2020-10-26 00:05:13 -0600

    Move methods into the Asset impl

--- dragonglass/src/gltf.rs
+++ dragonglass/src/gltf.rs
@@ -36,62 +36,6 @@ pub struct Vertex {
 
 const DEFAULT_NAME: &str = "<Unnamed>";
 
-pub fn load_gltf(path: &str) -> Result<Asset> {
-    let (gltf, buffers, textures) = gltf::import(&path)?;
-
-    let mut asset = Asset {
-        buffers,
-        textures,
-        ..Default::default()
-    };
-
-    for gltf_scene in gltf.scenes() {
-        load_scene(&gltf_scene, &mut asset)?;
-    }
-
-    Ok(asset)
-}
-
-fn load_scene(gltf_scene: &gltf::Scene, asset: &mut Asset) -> Result<()> {
-    let mut node_graphs = Vec::new();
-    for gltf_node in gltf_scene.nodes() {
-        let mut node_graph = NodeGraph::new();
-        load_node(&gltf_node, asset, &mut node_graph, NodeIndex::new(0))?;
-        node_graphs.push(node_graph);
-    }
-
-    let scene = Scene {
-        name: gltf_scene.name().unwrap_or(DEFAULT_NAME).to_string(),
-        node_graphs,
-    };
-    asset.scenes.push(scene);
-    Ok(())
-}
-
-fn load_node(
-    gltf_node: &gltf::Node,
-    asset: &mut Asset,
-    graph: &mut NodeGraph,
-    parent_index: NodeIndex,
-) -> Result<()> {
-    let node = Node {
-        name: gltf_node.name().unwrap_or(DEFAULT_NAME).to_string(),
-        transform: node_transform(gltf_node),
-        mesh: load_mesh(gltf_node, asset)?,
-    };
-
-    let index = graph.add_node(node);
-    if parent_index != index {
-        graph.add_edge(parent_index, index, ());
-    }
-
-    for child in gltf_node.children() {
-        load_node(&child, asset, graph, index)?;
-    }
-
-    Ok(())
-}
-
 fn node_transform(gltf_node: &gltf::Node) -> glm::Mat4 {
     let transform = gltf_node
         .transform()
@@ -101,93 +45,6 @@ fn node_transform(gltf_node: &gltf::Node) -> glm::Mat4 {
         .copied()
         .collect::<Vec<_>>();
     glm::make_mat4x4(&transform)
-}
-
-fn load_mesh(gltf_node: &gltf::Node, asset: &mut Asset) -> Result<Option<Mesh>> {
-    match gltf_node.mesh() {
-        Some(gltf_mesh) => {
-            let mut primitives = Vec::new();
-            for gltf_primitive in gltf_mesh.primitives() {
-                primitives.push(load_primitive(&gltf_primitive, asset)?);
-            }
-
-            let mesh = Mesh {
-                name: gltf_mesh.name().unwrap_or(DEFAULT_NAME).to_string(),
-                primitives,
-            };
-
-            Ok(Some(mesh))
-        }
-        None => Ok(None),
-    }
-}
-
-fn load_primitive(gltf_primitive: &gltf::Primitive, asset: &mut Asset) -> Result<Primitive> {
-    load_primitive_vertices(gltf_primitive, asset)?;
-    let first_index = asset.indices.len() as u32;
-    let number_of_indices = load_primitive_indices(gltf_primitive, asset)?;
-    Ok(Primitive {
-        first_index,
-        number_of_indices,
-    })
-}
-
-fn load_primitive_vertices(gltf_primitive: &gltf::Primitive, asset: &mut Asset) -> Result<()> {
-    let Asset {
-        buffers, vertices, ..
-    } = asset;
-
-    let reader = gltf_primitive.reader(|buffer| Some(&buffers[buffer.index()]));
-
-    let positions = reader
-        .read_positions()
-        .context("Failed to read vertex positions from the model. Vertex positions are required.")?
-        .map(glm::Vec3::from);
-    let number_of_vertices = positions.len();
-
-    let normals = reader.read_normals().map_or(
-        vec![glm::vec3(0.0, 0.0, 0.0); number_of_vertices],
-        |normals| normals.map(glm::Vec3::from).collect::<Vec<_>>(),
-    );
-
-    let map_to_vec2 = |coords: gltf::mesh::util::ReadTexCoords<'_>| -> Vec<glm::Vec2> {
-        coords.into_f32().map(glm::Vec2::from).collect::<Vec<_>>()
-    };
-    let uv_0 = reader
-        .read_tex_coords(0)
-        .map_or(vec![glm::vec2(0.0, 0.0); number_of_vertices], map_to_vec2);
-
-    for (index, position) in positions.into_iter().enumerate() {
-        vertices.push(Vertex {
-            position,
-            normal: normals[index],
-            uv_0: uv_0[index],
-        });
-    }
-
-    Ok(())
-}
-
-fn load_primitive_indices(gltf_primitive: &gltf::Primitive, asset: &mut Asset) -> Result<u32> {
-    let Asset { buffers, .. } = asset;
-
-    let reader = gltf_primitive.reader(|buffer| Some(&buffers[buffer.index()]));
-
-    let vertex_count = asset.vertices.len();
-    let primitive_indices = reader
-        .read_indices()
-        .map(|indices| {
-            indices
-                .into_u32()
-                .map(|x| x + vertex_count as u32)
-                .collect::<Vec<_>>()
-        })
-        .context("Failed to read indices!")?;
-
-    let number_of_indices = primitive_indices.len() as u32;
-    asset.indices.extend_from_slice(&primitive_indices);
-
-    Ok(number_of_indices)
 }
 
 fn global_transform(graph: &NodeGraph, index: NodeIndex) -> glm::Mat4 {
@@ -206,4 +63,151 @@ pub struct Asset {
     pub vertices: Vec<Vertex>,
     pub indices: Vec<u32>,
     pub scenes: Vec<Scene>,
+}
+
+impl Asset {
+    pub fn new(path: &str) -> Result<Self> {
+        let (gltf, buffers, textures) = gltf::import(&path)?;
+
+        let mut asset = Self {
+            buffers,
+            textures,
+            ..Default::default()
+        };
+
+        for gltf_scene in gltf.scenes() {
+            asset.load_scene(&gltf_scene)?;
+        }
+
+        Ok(asset)
+    }
+
+    fn load_scene(&mut self, gltf_scene: &gltf::Scene) -> Result<()> {
+        let mut node_graphs = Vec::new();
+        for gltf_node in gltf_scene.nodes() {
+            let mut node_graph = NodeGraph::new();
+            self.load_node(&gltf_node, &mut node_graph, NodeIndex::new(0))?;
+            node_graphs.push(node_graph);
+        }
+
+        let scene = Scene {
+            name: gltf_scene.name().unwrap_or(DEFAULT_NAME).to_string(),
+            node_graphs,
+        };
+        self.scenes.push(scene);
+        Ok(())
+    }
+
+    fn load_node(
+        &mut self,
+        gltf_node: &gltf::Node,
+        graph: &mut NodeGraph,
+        parent_index: NodeIndex,
+    ) -> Result<()> {
+        let node = Node {
+            name: gltf_node.name().unwrap_or(DEFAULT_NAME).to_string(),
+            transform: node_transform(gltf_node),
+            mesh: self.load_mesh(gltf_node)?,
+        };
+
+        let index = graph.add_node(node);
+        if parent_index != index {
+            graph.add_edge(parent_index, index, ());
+        }
+
+        for child in gltf_node.children() {
+            self.load_node(&child, graph, index)?;
+        }
+
+        Ok(())
+    }
+
+    fn load_mesh(&mut self, gltf_node: &gltf::Node) -> Result<Option<Mesh>> {
+        match gltf_node.mesh() {
+            Some(gltf_mesh) => {
+                let mut primitives = Vec::new();
+                for gltf_primitive in gltf_mesh.primitives() {
+                    primitives.push(self.load_primitive(&gltf_primitive)?);
+                }
+
+                let mesh = Mesh {
+                    name: gltf_mesh.name().unwrap_or(DEFAULT_NAME).to_string(),
+                    primitives,
+                };
+
+                Ok(Some(mesh))
+            }
+            None => Ok(None),
+        }
+    }
+
+    fn load_primitive(&mut self, gltf_primitive: &gltf::Primitive) -> Result<Primitive> {
+        self.load_primitive_vertices(gltf_primitive)?;
+        let first_index = self.indices.len() as u32;
+        let number_of_indices = self.load_primitive_indices(gltf_primitive)?;
+        Ok(Primitive {
+            first_index,
+            number_of_indices,
+        })
+    }
+
+    fn load_primitive_vertices(&mut self, gltf_primitive: &gltf::Primitive) -> Result<()> {
+        let Self {
+            buffers, vertices, ..
+        } = self;
+
+        let reader = gltf_primitive.reader(|buffer| Some(&buffers[buffer.index()]));
+
+        let positions = reader
+            .read_positions()
+            .context(
+                "Failed to read vertex positions from the model. Vertex positions are required.",
+            )?
+            .map(glm::Vec3::from);
+        let number_of_vertices = positions.len();
+
+        let normals = reader.read_normals().map_or(
+            vec![glm::vec3(0.0, 0.0, 0.0); number_of_vertices],
+            |normals| normals.map(glm::Vec3::from).collect::<Vec<_>>(),
+        );
+
+        let map_to_vec2 = |coords: gltf::mesh::util::ReadTexCoords<'_>| -> Vec<glm::Vec2> {
+            coords.into_f32().map(glm::Vec2::from).collect::<Vec<_>>()
+        };
+        let uv_0 = reader
+            .read_tex_coords(0)
+            .map_or(vec![glm::vec2(0.0, 0.0); number_of_vertices], map_to_vec2);
+
+        for (index, position) in positions.into_iter().enumerate() {
+            vertices.push(Vertex {
+                position,
+                normal: normals[index],
+                uv_0: uv_0[index],
+            });
+        }
+
+        Ok(())
+    }
+
+    fn load_primitive_indices(&mut self, gltf_primitive: &gltf::Primitive) -> Result<u32> {
+        let Self { buffers, .. } = self;
+
+        let reader = gltf_primitive.reader(|buffer| Some(&buffers[buffer.index()]));
+
+        let vertex_count = self.vertices.len();
+        let primitive_indices = reader
+            .read_indices()
+            .map(|indices| {
+                indices
+                    .into_u32()
+                    .map(|x| x + vertex_count as u32)
+                    .collect::<Vec<_>>()
+            })
+            .context("Failed to read indices!")?;
+
+        let number_of_indices = primitive_indices.len() as u32;
+        self.indices.extend_from_slice(&primitive_indices);
+
+        Ok(number_of_indices)
+    }
 }
