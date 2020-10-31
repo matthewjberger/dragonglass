606069edd93ed456dda436747ad1791ee2e55891
matthewjberger <matthewjberger@nevada.unr.edu>
2020-10-31 11:18:56 -0600

    Use emissive maps in shader

--- assets/shaders/gltf/gltf.frag.glsl
+++ assets/shaders/gltf/gltf.frag.glsl
@@ -2,11 +2,11 @@
 
 #define MAX_NUMBER_OF_TEXTURES 100
 
-layout(location=0)in vec2 inUV0;
+layout(location=0) in vec2 inUV0;
 
-layout(binding=2)uniform sampler2D textures[MAX_NUMBER_OF_TEXTURES];
+layout(binding=2) uniform sampler2D textures[MAX_NUMBER_OF_TEXTURES];
 
-layout(push_constant)uniform Material{
+layout(push_constant) uniform Material{
     vec4 baseColorFactor;
     vec3 emissiveFactor;
     int colorTextureSet;
@@ -18,35 +18,40 @@ layout(push_constant)uniform Material{
     float roughnessFactor;
     int alphaMode;
     float alphaCutoff;
-}material;
+} material;
 
-layout(location=0)out vec4 outColor;
+layout(location = 0) out vec4 outColor;
 
-layout(binding=0)uniform UboView{
+layout(binding = 0) uniform UboView{
     mat4 view;
     mat4 projection;
-}uboView;
+} uboView;
 
 vec4 srgb_to_linear(vec4 srgbIn)
 {
-    vec3 linOut=pow(srgbIn.xyz,vec3(2.2));
+    vec3 linOut = pow(srgbIn.xyz,vec3(2.2));
     return vec4(linOut,srgbIn.w);;
 }
 
 void main()
 {
     vec4 baseColor;
-    
-    if(material.colorTextureSet>-1){
-        vec4 albedoMap=texture(textures[material.colorTextureSet],inUV0);
-        baseColor=srgb_to_linear(albedoMap)*material.baseColorFactor;
-    }else{
-        baseColor=material.baseColorFactor;
+    if(material.colorTextureSet > -1) {
+        vec4 albedoMap = texture(textures[material.colorTextureSet],inUV0);
+        baseColor = srgb_to_linear(albedoMap) * material.baseColorFactor;
+    } else {
+        baseColor = material.baseColorFactor;
     }
     
-    if(material.alphaMode==2 && baseColor.a<material.alphaCutoff){
+    if(material.alphaMode == 2 && baseColor.a < material.alphaCutoff) {
         discard;
     }
+
+    vec3 color = baseColor.rgb;
+    if(material.emissiveTextureSet > -1) {
+        vec4 emissiveMap = texture(textures[material.emissiveTextureSet], inUV0);
+        color += srgb_to_linear(emissiveMap).rgb * material.emissiveFactor;
+    }
   
-    outColor=baseColor;
+    outColor = vec4(color, baseColor.a);
 }
