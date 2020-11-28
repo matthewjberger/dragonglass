3f88967525c446def8053994b4eb93c8c6010182
Matthew J. Berger <matthewberger@nevada.unr.edu>
2020-11-28 15:59:54 -0700

    Add lifetime specifier

--- viewer/src/state.rs
+++ viewer/src/state.rs
@@ -1,13 +1,19 @@
 use anyhow::Result;
 use dragonglass_world::World;
 
-pub enum Transition<T, E> {
+pub struct StateData<'a, T, E> {
+    world: &'a World,
+    event: &'a E,
+    data: &'a T,
+}
+
+pub enum Transition<'a, T, E> {
     None,
-    Push(Box<dyn State<T, E>>),
+    Push(Box<dyn State<'a, T, E>>),
     Pop,
 }
 
-pub trait State<T, E> {
+pub trait State<'a, T, E> {
     fn initialize(&mut self, world: &mut World, data: &mut T, event: &E) -> Result<()> {
         Ok(())
     }
@@ -24,16 +30,21 @@ pub trait State<T, E> {
         Ok(())
     }
 
-    fn update(&mut self, world: &mut World, data: &mut T, event: &E) -> Result<Transition<T, E>> {
+    fn update(
+        &mut self,
+        world: &mut World,
+        data: &mut T,
+        event: &E,
+    ) -> Result<Transition<'a, T, E>> {
         Ok(Transition::None)
     }
 }
 
-pub struct StateMachine<T, E> {
-    states: Vec<Box<dyn State<T, E>>>,
+pub struct StateMachine<'a, T, E> {
+    states: Vec<Box<dyn State<'a, T, E>>>,
 }
 
-impl<T, E> StateMachine<T, E> {
+impl<'a, T, E> StateMachine<'a, T, E> {
     pub fn new() -> Self {
         Self { states: Vec::new() }
     }
@@ -43,7 +54,12 @@ impl<T, E> StateMachine<T, E> {
         world: &mut World,
         data: &mut T,
         event: &E,
-        action: impl Fn(&mut Box<dyn State<T, E>>, &mut World, &mut T, &E) -> Result<Transition<T, E>>,
+        action: impl Fn(
+            &mut Box<dyn State<'a, T, E>>,
+            &mut World,
+            &mut T,
+            &E,
+        ) -> Result<Transition<'a, T, E>>,
     ) -> Result<()> {
         if let Some(state) = self.states.last_mut() {
             let transition = action(state, world, data, event)?;
@@ -74,7 +90,7 @@ impl<T, E> StateMachine<T, E> {
 
     pub fn transition(
         &mut self,
-        transition: Transition<T, E>,
+        transition: Transition<'a, T, E>,
         world: &mut World,
         data: &mut T,
         event: &E,
@@ -88,7 +104,7 @@ impl<T, E> StateMachine<T, E> {
 
     pub fn push(
         &mut self,
-        state: Box<dyn State<T, E>>,
+        state: Box<dyn State<'a, T, E>>,
         world: &mut World,
         data: &mut T,
         event: &E,
