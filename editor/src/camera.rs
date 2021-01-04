e2c1b4862c08ad1147e397286ac0cde42160dbe4
Matthew J. Berger <matthewberger@nevada.unr.edu>
2021-01-04 00:35:25 -0700

    Track arcball offset

--- editor/src/camera.rs
+++ editor/src/camera.rs
@@ -4,7 +4,7 @@ use nalgebra_glm as glm;
 
 #[derive(Default)]
 pub(crate) struct Arcball {
-    pub offset: glm::Vec3, // TODO: this needs to track the arcball target
+    pub offset: glm::Vec3,
 }
 
 impl Arcball {
@@ -27,13 +27,12 @@ impl Arcball {
         if application.input.mouse.is_right_clicked {
             transform.translation -= right * mouse_delta.x * delta_time;
             transform.translation += up * mouse_delta.y * delta_time;
+            self.offset -= right * mouse_delta.x * delta_time;
+            self.offset += up * mouse_delta.y * delta_time;
         }
 
         if application.input.mouse.is_left_clicked {
-            let yaw_delta = -mouse_delta.x * delta_time;
-            transform.translation =
-                glm::rotate_vec3(&transform.translation, yaw_delta, &glm::Vec3::y());
-
+            // Pitch
             let pitch_bound = 80_f32.to_radians();
             let pitch = glm::quat_euler_angles(&transform.rotation).z;
             let mut pitch_delta = -mouse_delta.y * delta_time;
@@ -46,7 +45,21 @@ impl Arcball {
             transform.translation =
                 glm::rotate_vec3(&transform.translation, pitch_delta, &transform.right());
 
-            let target = -transform.translation;
+            // Yaw
+            let yaw_bound = 80_f32.to_radians();
+            let yaw = glm::quat_euler_angles(&transform.rotation).y;
+            let mut yaw_delta = -mouse_delta.x * delta_time;
+            if yaw + yaw_delta > yaw_bound {
+                yaw_delta = yaw_bound - yaw;
+            }
+            if yaw + yaw_delta < -yaw_bound {
+                yaw_delta = -yaw_bound - yaw;
+            }
+            transform.translation =
+                glm::rotate_vec3(&transform.translation, yaw_delta, &glm::Vec3::y());
+
+            // Foxus on target
+            let target = -(transform.translation - self.offset);
             transform.look_at(&target, &glm::Vec3::y());
         }
 
