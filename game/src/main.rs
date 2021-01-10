34a1a997bc98e7cd5068321d9b41efbb8c8024a4
Matthew J. Berger <matthewberger@nevada.unr.edu>
2021-01-09 18:12:10 -0700

    Add some physics and controls

--- game/src/main.rs
+++ game/src/main.rs
@@ -3,11 +3,12 @@ use dragonglass::{
     app::Application,
     app::{run_application, AppConfig, ApplicationRunner},
     physics::RigidBody,
-    world::{Entity, Mesh, Transform},
+    world::{Camera, Entity, Hidden, Mesh, PerspectiveCamera, Projection, Transform},
 };
 use imgui::{im_str, Condition, Ui, Window};
 use nalgebra_glm as glm;
 use rapier3d::{dynamics::BodyStatus, dynamics::RigidBodyBuilder, geometry::ColliderBuilder};
+use winit::event::{ElementState, VirtualKeyCode};
 
 #[derive(Default)]
 pub struct Game {
@@ -26,8 +27,10 @@ impl ApplicationRunner for Game {
             if mesh.name == "mesh_helmet_LP_13930damagedHelmet" {
                 self.helmet = Some(entity);
                 {
-                    let mut transform = application.ecs.get_mut::<Transform>(entity)?;
-                    transform.translation.y = 200.0;
+                    {
+                        let mut transform = application.ecs.get_mut::<Transform>(entity)?;
+                        transform.translation.y = 200.0;
+                    }
                 }
             }
             if mesh.name == "Cylinder" {
@@ -47,16 +50,44 @@ impl ApplicationRunner for Game {
             }
         }
 
+        // Disable active camera
+        // let camera_entity = application.world.active_camera(&mut application.ecs)?;
+        // application.ecs.get_mut::<Camera>(camera_entity)?.enabled = false;
+
         if let Some(entity) = self.helmet.as_ref() {
             add_rigid_body(*entity, application, BodyStatus::Dynamic)?;
         }
         if let Some(entity) = self.deer.as_ref() {
+            // application.ecs.insert_one(*entity, Hidden {})?;
+            // application.ecs.insert_one(
+            //     *entity,
+            //     Camera {
+            //         name: "Player Camera".to_string(),
+            //         projection: Projection::Perspective(PerspectiveCamera {
+            //             aspect_ratio: None,
+            //             y_fov_rad: 70_f32.to_radians(),
+            //             z_far: Some(1000.0),
+            //             z_near: 0.1,
+            //         }),
+            //         enabled: true,
+            //     },
+            // )?;
             add_rigid_body(*entity, application, BodyStatus::Dynamic)?;
         }
         if let Some(entity) = self.plane.as_ref() {
             add_rigid_body(*entity, application, BodyStatus::Static)?;
         }
 
+        Ok(())
+    }
+
+    fn create_ui(&mut self, _application: &mut Application, ui: &Ui) -> Result<()> {
+        Window::new(im_str!("Physics Test"))
+            .size([100.0, 40.0], Condition::FirstUseEver)
+            .no_decoration()
+            .build(ui, || {
+                ui.text(im_str!("Physics test"));
+            });
         Ok(())
     }
 
@@ -71,16 +102,58 @@ impl ApplicationRunner for Game {
                 transform.rotation = *position.rotation.quaternion();
             }
         }
+
+        let speed = 6.0 * application.system.delta_time as f32;
+        if let Some(entity) = self.deer.as_ref() {
+            {
+                let mut transform = application.ecs.get_mut::<Transform>(*entity)?;
+                let mut translation = glm::vec3(0.0, 0.0, 0.0);
+
+                if application.input.is_key_pressed(VirtualKeyCode::W) {
+                    translation = speed * transform.forward();
+                }
+
+                if application.input.is_key_pressed(VirtualKeyCode::A) {
+                    translation = -speed * transform.right();
+                }
+
+                if application.input.is_key_pressed(VirtualKeyCode::S) {
+                    translation = -speed * transform.forward();
+                }
+
+                if application.input.is_key_pressed(VirtualKeyCode::D) {
+                    translation = speed * transform.right();
+                }
+
+                transform.translation += translation;
+            }
+            sync_rigid_body_to_transform(application, *entity)?;
+        }
+
         Ok(())
     }
 
-    fn create_ui(&mut self, _application: &mut Application, ui: &Ui) -> Result<()> {
-        Window::new(im_str!("Physics Test"))
-            .size([100.0, 40.0], Condition::FirstUseEver)
-            .no_decoration()
-            .build(ui, || {
-                ui.text(im_str!("Physics test"));
-            });
+    fn on_key(
+        &mut self,
+        application: &mut Application,
+        keystate: ElementState,
+        keycode: VirtualKeyCode,
+    ) -> Result<()> {
+        if let Some(entity) = self.deer.as_ref() {
+            if let (VirtualKeyCode::Space, ElementState::Pressed) = (keycode, keystate) {
+                if let Some(entity) = self.deer.as_ref() {
+                    let rigid_body_handle = application.ecs.get::<RigidBody>(*entity)?.handle;
+                    if let Some(rigid_body) =
+                        application.physics_world.bodies.get_mut(rigid_body_handle)
+                    {
+                        let jump_strength = 40.0;
+                        let impulse = jump_strength * glm::Vec3::y();
+                        rigid_body.apply_impulse(impulse, true);
+                    }
+                }
+            }
+            sync_transform_to_rigid_body(application, *entity)?;
+        }
         Ok(())
     }
 }
@@ -137,5 +210,28 @@ fn add_rigid_body(
         handle
     };
     application.ecs.insert_one(entity, RigidBody::new(handle))?;
+    Ok(())
+}
+
+fn sync_rigid_body_to_transform(application: &mut Application, entity: Entity) -> Result<()> {
+    let rigid_body_handle = application.ecs.get::<RigidBody>(entity)?.handle;
+    let transform = application.ecs.get::<Transform>(entity)?;
+    if let Some(body) = application.physics_world.bodies.get_mut(rigid_body_handle) {
+        body.set_position(transform.as_isometry(), false);
+    }
+    Ok(())
+}
+
+fn sync_transform_to_rigid_body(application: &mut Application, entity: Entity) -> Result<()> {
+    let rigid_body_handle = application.ecs.get::<RigidBody>(entity)?.handle;
+    let mut transform = application.ecs.get_mut::<Transform>(entity)?;
+    if let Some(body) = application.physics_world.bodies.get(rigid_body_handle) {
+        let position = body.position();
+        transform.translation = position.translation.vector;
+        transform.rotation = *position.rotation.quaternion();
+    }
+    if let Some(body) = application.physics_world.bodies.get_mut(rigid_body_handle) {
+        body.wake_up(false);
+    }
     Ok(())
 }
