0f5f0d29bf055087e47054f5a4145ed936e67125
Matthew J. Berger <matthewberger@nevada.unr.edu>
2021-08-23 22:43:20 -0600

    Add vertex descriptor for world vertices

--- crates/dragonglass_render/src/world.rs
+++ crates/dragonglass_render/src/world.rs
@@ -1,5 +1,6 @@
-use anyhow::Result;
+use anyhow::{bail, Result};
 use dragonglass_world::World;
+use wgpu::util::DeviceExt;
 
 pub struct WorldRender {
     pub vertex_buffer: wgpu::Buffer,
@@ -8,7 +9,80 @@ pub struct WorldRender {
 }
 
 impl WorldRender {
-    pub fn new(world: &World) -> Result<Self> {
-        unimplemented!()
+    pub fn new(device: &wgpu::Device, world: &World) -> Self {
+        let vertex_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("World Vertex Buffer"),
+            contents: bytemuck::cast_slice(&world.geometry.vertices),
+            usage: wgpu::BufferUsages::VERTEX,
+        });
+
+        let index_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Index Buffer"),
+            contents: bytemuck::cast_slice(&world.geometry.indices),
+            usage: wgpu::BufferUsages::INDEX,
+        });
+
+        let shader = device.create_shader_module(&wgpu::ShaderModuleDescriptor {
+            label: Some("Shader"),
+            source: wgpu::ShaderSource::Wgsl(include_str!("shaders/shader.wgsl").into()),
+        });
+
+        Self {
+            vertex_buffer,
+            index_buffer,
+            shader,
+        }
+    }
+}
+
+pub fn vertex_descriptor<'a>() -> wgpu::VertexBufferLayout<'a> {
+    wgpu::VertexBufferLayout {
+        array_stride: std::mem::size_of::<dragonglass_world::Vertex>() as wgpu::BufferAddress,
+        step_mode: wgpu::VertexStepMode::Vertex,
+        // [3, 3, 2, 2, 4, 4, 3]
+        attributes: &[
+            // Position
+            wgpu::VertexAttribute {
+                offset: 0,
+                shader_location: 0,
+                format: wgpu::VertexFormat::Float32x3,
+            },
+            // Normal
+            wgpu::VertexAttribute {
+                offset: std::mem::size_of::<[f32; 3]>() as wgpu::BufferAddress,
+                shader_location: 1,
+                format: wgpu::VertexFormat::Float32x3,
+            },
+            // UV_0
+            wgpu::VertexAttribute {
+                offset: std::mem::size_of::<[f32; 6]>() as wgpu::BufferAddress,
+                shader_location: 2,
+                format: wgpu::VertexFormat::Float32x2,
+            },
+            // UV_1
+            wgpu::VertexAttribute {
+                offset: std::mem::size_of::<[f32; 8]>() as wgpu::BufferAddress,
+                shader_location: 3,
+                format: wgpu::VertexFormat::Float32x2,
+            },
+            // JOINT_0
+            wgpu::VertexAttribute {
+                offset: std::mem::size_of::<[f32; 10]>() as wgpu::BufferAddress,
+                shader_location: 4,
+                format: wgpu::VertexFormat::Float32x4,
+            },
+            // JOINT_1
+            wgpu::VertexAttribute {
+                offset: std::mem::size_of::<[f32; 14]>() as wgpu::BufferAddress,
+                shader_location: 5,
+                format: wgpu::VertexFormat::Float32x4,
+            },
+            // COLOR_0
+            wgpu::VertexAttribute {
+                offset: std::mem::size_of::<[f32; 18]>() as wgpu::BufferAddress,
+                shader_location: 6,
+                format: wgpu::VertexFormat::Float32x3,
+            },
+        ],
     }
 }
