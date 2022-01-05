cfb7e75c9719f447e44e5275ab4fce342267c0c3
Matthew J. Berger <matthewjordanberger@gmail.com>
2022-01-05 00:42:35 -0700

    Add gui crate

---
+++ crates/dragonglass_gui/src/gui.rs
@@ -0,0 +1,81 @@
+use egui::{ClippedMesh, CtxRef, FontDefinitions};
+use egui_winit_platform::{Platform, PlatformDescriptor};
+use std::{sync::Arc, time::Instant};
+use winit::{event::Event, window::Window};
+
+pub struct ScreenDescriptor {
+    pub width: u32,
+    pub height: u32,
+    pub scale_factor: f32,
+}
+
+// We repaint the UI every frame, so no custom repaint signal is needed
+struct RepaintSignal;
+impl epi::backend::RepaintSignal for RepaintSignal {
+    fn request_repaint(&self) {}
+}
+
+pub struct Gui {
+    platform: Platform,
+    repaint_signal: Arc<RepaintSignal>,
+    start_time: Instant,
+    last_frame_start: Instant,
+    previous_frame_time: Option<f32>,
+}
+
+impl Gui {
+    pub fn new(screen_descriptor: ScreenDescriptor) -> Self {
+        let platform = Platform::new(PlatformDescriptor {
+            physical_width: screen_descriptor.width,
+            physical_height: screen_descriptor.height,
+            scale_factor: screen_descriptor.scale_factor as _,
+            font_definitions: FontDefinitions::default(),
+            style: Default::default(),
+        });
+
+        Self {
+            platform,
+            repaint_signal: std::sync::Arc::new(RepaintSignal {}),
+            start_time: Instant::now(),
+            previous_frame_time: None,
+            last_frame_start: Instant::now(),
+        }
+    }
+
+    pub fn handle_event(&mut self, event: &Event<()>) {
+        self.platform.handle_event(&event);
+    }
+
+    pub fn context(&self) -> CtxRef {
+        self.platform.context()
+    }
+
+    pub fn start_frame<'a>(&mut self, scale_factor: f32) -> epi::backend::FrameData {
+        self.platform
+            .update_time(self.start_time.elapsed().as_secs_f64());
+
+        // Begin to draw the UI frame.
+        self.last_frame_start = Instant::now();
+        self.platform.begin_frame();
+        let app_output = epi::backend::AppOutput::default();
+
+        epi::backend::FrameData {
+            info: epi::IntegrationInfo {
+                name: "egui_frame",
+                web_info: None,
+                cpu_usage: self.previous_frame_time,
+                native_pixels_per_point: Some(scale_factor),
+                prefer_dark_mode: None,
+            },
+            output: app_output,
+            repaint_signal: self.repaint_signal.clone(),
+        }
+    }
+
+    pub fn end_frame(&mut self, window: &Window) -> Vec<ClippedMesh> {
+        let (_output, paint_commands) = self.platform.end_frame(Some(&window));
+        let frame_time = (Instant::now() - self.last_frame_start).as_secs_f64() as f32;
+        self.previous_frame_time = Some(frame_time);
+        self.platform.context().tessellate(paint_commands)
+    }
+}
