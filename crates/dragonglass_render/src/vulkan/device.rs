261a445b2d76cdd6f067c6f76d2f4eda8d00d918
Matthew J. Berger <matthewjordanberger@gmail.com>
2022-01-30 18:55:03 -0700

    Start refactoring scene

--- crates/dragonglass_render/src/vulkan/device.rs
+++ crates/dragonglass_render/src/vulkan/device.rs
@@ -53,105 +53,35 @@ impl Renderer for VulkanRenderBackend {
         let Self { frame, scene, .. } = self;
 
         let aspect_ratio = frame.swapchain_properties.aspect_ratio();
-        let device = self.context.device.clone();
+        scene.update(
+            &world,
+            aspect_ratio,
+            &self.context,
+            gui_context,
+            &clipped_meshes,
+        )?;
 
-        let (projection, view) = world.active_camera_matrices(aspect_ratio)?;
-        let camera_entity = world.active_camera()?;
-        let camera_transform = world.entity_global_transform(camera_entity)?;
-
-        // Maintain a perspective projection for the skybox
-        let using_ortho_projection = world
-            .ecs
-            .entry_ref(camera_entity)?
-            .get_component::<Camera>()?
-            .is_orthographic();
-        let skybox_projection = if using_ortho_projection {
-            let camera = PerspectiveCamera {
-                aspect_ratio: None,
-                y_fov_rad: 70_f32.to_radians(),
-                z_far: Some(1000.0),
-                z_near: 0.01,
-            };
-            camera.matrix(aspect_ratio)
-        } else {
-            projection
-        };
-
-        scene.update(&self.context, gui_context, &clipped_meshes)?;
-
+        let context = &self.context;
         let viewport = self.viewport;
         frame.render(viewport, |command_buffer, image_index| {
-            if let Some(world_render) = scene.world_render.as_mut() {
-                world_render.pbr_pipeline_data.update_dynamic_ubo(world)?;
-
-                let (lights, number_of_lights) = load_lights(world)?;
-
-                let mut joint_matrices =
-                    [glm::Mat4::identity(); PbrPipelineData::MAX_NUMBER_OF_JOINTS];
-                joint_matrices
-                    .iter_mut()
-                    .zip(world.joint_matrices()?.into_iter())
-                    .for_each(|(a, b)| *a = b);
-
-                let ubo = WorldUniformBuffer {
-                    view,
-                    projection,
-                    camera_position: camera_transform.translation,
-                    number_of_lights,
-                    lights,
-                    joint_matrices,
-                };
-                world_render
-                    .pbr_pipeline_data
-                    .uniform_buffer
-                    .upload_data(&[ubo], 0)?;
-            }
-
-            scene.skybox_render.projection = skybox_projection;
-            scene.skybox_render.view = view;
-
-            scene.rendergraph.execute_pass(
+            // TODO: Make this take less parameters...
+            scene.execute_passes(
+                context,
                 command_buffer,
-                "offscreen",
+                &world,
                 image_index,
-                |pass, command_buffer| {
-                    device.update_viewport(command_buffer, pass.extent, true)?;
-                    scene.skybox_render.issue_commands(command_buffer)?;
-                    if let Some(world_render) = scene.world_render.as_ref() {
-                        world_render.issue_commands(command_buffer, world, aspect_ratio)?;
-                    }
-                    Ok(())
-                },
-            )?;
-
-            scene.rendergraph.execute_pass(
-                command_buffer,
-                "fullscreen",
-                image_index,
-                |pass, command_buffer| {
-                    device.update_viewport(command_buffer, pass.extent, false)?;
-                    if let Some(fullscreen_pipeline) = scene.fullscreen_pipeline.as_ref() {
-                        fullscreen_pipeline.issue_commands(command_buffer)?;
-                    }
-                    scene
-                        .gui_render
-                        .issue_commands(viewport, command_buffer, &clipped_meshes)?;
-                    Ok(())
-                },
-            )?;
-
-            Ok(())
+                aspect_ratio,
+                viewport,
+                &clipped_meshes,
+            )
         })?;
 
         if frame.recreated_swapchain {
-            let rendergraph = Scene::create_rendergraph(
+            scene.recreate_rendergraph(
                 &self.context,
                 frame.swapchain()?,
                 &frame.swapchain_properties,
-                scene.samples,
             )?;
-            scene.rendergraph = rendergraph;
-            scene.create_pipelines(&self.context)?;
         }
 
         Ok(())
@@ -174,19 +104,4 @@ impl Drop for VulkanRenderBackend {
             }
         }
     }
-}
-
-fn load_lights(world: &World) -> Result<([Light; PbrPipelineData::MAX_NUMBER_OF_LIGHTS], u32)> {
-    let mut lights = [Light::default(); PbrPipelineData::MAX_NUMBER_OF_LIGHTS];
-    let world_lights = world
-        .lights()?
-        .iter()
-        .map(|(transform, light)| Light::from_node(transform, light))
-        .collect::<Vec<_>>();
-    let number_of_lights = world_lights.len() as u32;
-    lights
-        .iter_mut()
-        .zip(world_lights)
-        .for_each(|(a, b)| *a = b);
-    Ok((lights, number_of_lights))
 }
--- crates/dragonglass_render/src/vulkan/scene.rs
+++ crates/dragonglass_render/src/vulkan/scene.rs
@@ -2,7 +2,7 @@ use crate::vulkan::world::WorldRender;
 use anyhow::Result;
 use dragonglass_gui::egui::{ClippedMesh, CtxRef};
 use dragonglass_vulkan::{
-    ash::vk,
+    ash::vk::{self, CommandBuffer},
     core::{
         CommandPool, Context, Device, Image, ImageNode, RawImage, RenderGraph, ShaderCache,
         ShaderPathSetBuilder, Swapchain, SwapchainProperties,
@@ -10,10 +10,14 @@ use dragonglass_vulkan::{
     pbr::EnvironmentMapSet,
     render::{FullscreenRender, SkyboxRender},
 };
-use dragonglass_world::World;
+use dragonglass_world::{Camera, EntityStore, PerspectiveCamera, Viewport, World};
+use nalgebra_glm as glm;
 use std::sync::Arc;
 
-use super::gui::GuiRender;
+use super::{
+    gui::GuiRender,
+    world::{Light, PbrPipelineData, WorldUniformBuffer},
+};
 
 pub struct Scene {
     pub environment_maps: EnvironmentMapSet,
@@ -257,8 +261,23 @@ impl Scene {
         Ok(())
     }
 
+    pub fn recreate_rendergraph(
+        &mut self,
+        context: &Context,
+        swapchain: &Swapchain,
+        swapchain_properties: &SwapchainProperties,
+    ) -> Result<()> {
+        let rendergraph =
+            Scene::create_rendergraph(&context, swapchain, swapchain_properties, self.samples)?;
+        self.rendergraph = rendergraph;
+        self.create_pipelines(context)?;
+        Ok(())
+    }
+
     pub fn update(
         &mut self,
+        world: &World,
+        aspect_ratio: f32,
         context: &Context,
         gui_context: Option<&CtxRef>,
         clipped_meshes: &[ClippedMesh],
@@ -271,6 +290,114 @@ impl Scene {
                 clipped_meshes,
             )?;
         }
+
+        let (projection, view) = world.active_camera_matrices(aspect_ratio)?;
+        let camera_entity = world.active_camera()?;
+        let camera_transform = world.entity_global_transform(camera_entity)?;
+
+        // Maintain a perspective projection for the skybox
+        let using_ortho_projection = world
+            .ecs
+            .entry_ref(camera_entity)?
+            .get_component::<Camera>()?
+            .is_orthographic();
+        let skybox_projection = if using_ortho_projection {
+            let camera = PerspectiveCamera {
+                aspect_ratio: None,
+                y_fov_rad: 70_f32.to_radians(),
+                z_far: Some(1000.0),
+                z_near: 0.01,
+            };
+            camera.matrix(aspect_ratio)
+        } else {
+            projection
+        };
+
+        self.skybox_render.projection = skybox_projection;
+        self.skybox_render.view = view;
+
+        if let Some(world_render) = self.world_render.as_mut() {
+            world_render.pbr_pipeline_data.update_dynamic_ubo(world)?;
+            let (lights, number_of_lights) = Self::load_lights(world)?;
+
+            let mut joint_matrices = [glm::Mat4::identity(); PbrPipelineData::MAX_NUMBER_OF_JOINTS];
+            joint_matrices
+                .iter_mut()
+                .zip(world.joint_matrices()?.into_iter())
+                .for_each(|(a, b)| *a = b);
+
+            let ubo = WorldUniformBuffer {
+                view,
+                projection,
+                camera_position: camera_transform.translation,
+                number_of_lights,
+                lights,
+                joint_matrices,
+            };
+            world_render
+                .pbr_pipeline_data
+                .uniform_buffer
+                .upload_data(&[ubo], 0)?;
+        }
+
+        Ok(())
+    }
+
+    fn load_lights(world: &World) -> Result<([Light; PbrPipelineData::MAX_NUMBER_OF_LIGHTS], u32)> {
+        let mut lights = [Light::default(); PbrPipelineData::MAX_NUMBER_OF_LIGHTS];
+        let world_lights = world
+            .lights()?
+            .iter()
+            .map(|(transform, light)| Light::from_node(transform, light))
+            .collect::<Vec<_>>();
+        let number_of_lights = world_lights.len() as u32;
+        lights
+            .iter_mut()
+            .zip(world_lights)
+            .for_each(|(a, b)| *a = b);
+        Ok((lights, number_of_lights))
+    }
+
+    pub fn execute_passes(
+        &mut self,
+        context: &Context,
+        command_buffer: CommandBuffer,
+        world: &World,
+        image_index: usize,
+        aspect_ratio: f32,
+        viewport: Viewport,
+        clipped_meshes: &[ClippedMesh],
+    ) -> Result<()> {
+        let device = context.device.clone();
+        self.rendergraph.execute_pass(
+            command_buffer,
+            "offscreen",
+            image_index,
+            |pass, command_buffer| {
+                device.update_viewport(command_buffer, pass.extent, true)?;
+                self.skybox_render.issue_commands(command_buffer)?;
+                if let Some(world_render) = self.world_render.as_ref() {
+                    world_render.issue_commands(command_buffer, world, aspect_ratio)?;
+                }
+                Ok(())
+            },
+        )?;
+
+        self.rendergraph.execute_pass(
+            command_buffer,
+            "fullscreen",
+            image_index,
+            |pass, command_buffer| {
+                device.update_viewport(command_buffer, pass.extent, false)?;
+                if let Some(fullscreen_pipeline) = self.fullscreen_pipeline.as_ref() {
+                    fullscreen_pipeline.issue_commands(command_buffer)?;
+                }
+                self.gui_render
+                    .issue_commands(viewport, command_buffer, &clipped_meshes)?;
+                Ok(())
+            },
+        )?;
+
         Ok(())
     }
 }
